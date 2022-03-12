86e86415db45dacdacb6cb942dd346553de6797d
Carlos Gabriel <carlosgmr2304@gmail.com>
2022-03-12 13:44:50 -0300

    Criando o Core Module
    Criando um modulo para os componentes principais da aplicação.

--- algamoney-ui/src/app/app.module.ts
+++ algamoney-ui/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 import { PessoasModule } from './pessoas/pessoas.module';
 import { LancamentoModule } from './lancamento/lancamento.module';
@@ -7,14 +8,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
 
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavbarComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
@@ -24,7 +23,9 @@ import { NavbarComponent } from './navbar/navbar.component';
 
     PessoasModule,
 
-    SharedModule
+    SharedModule,
+
+    CoreModule
 
   ],
   providers: [],
