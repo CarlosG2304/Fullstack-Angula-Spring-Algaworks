e91560f6ff8ad24d8bc59ffcc36dd46888898c1d
Carlos Gabriel <carlosgmr2304@gmail.com>
2022-03-25 11:04:15 -0300

    Refatorando as rotas para usar Routing Module

--- algamoney-ui/src/app/app.module.ts
+++ algamoney-ui/src/app/app.module.ts
@@ -1,35 +1,22 @@
-import { Routes, RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
-import { PessoasPesquisaComponent } from './pessoas/pessoas-pesquisa/pessoas-pesquisa.component';
-import { LancamentoCadastroComponent } from './lancamento/lancamento-cadastro/lancamento-cadastro.component';
-import { LancamentosPesquisaComponent } from './lancamento/lancamentos-pesquisa/lancamentos-pesquisa.component';
 import { CategoriasService } from './categorias/categorias.service';
 import { PessoasService } from './pessoas/pessoas.service';
 import { LancamentoService } from './lancamento/lancamento.service';
-import { PaginaNaoEncontradaComponent } from './core/pagina-nao-encontrada.component';
 
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 import { PessoasModule } from './pessoas/pessoas.module';
 import { LancamentoModule } from './lancamento/lancamento.module';
+import { AppRoutingModule } from './app-routing.module';
 
 
 import { AppComponent } from './app.component';
 
-const routes: Routes = [
-  { path: '', redirectTo: 'lancamentos', pathMatch:'full'},
-  { path: 'lancamentos', component: LancamentosPesquisaComponent},
-  { path: 'lancamentos/novo', component: LancamentoCadastroComponent},
-  { path: 'lancamentos/:codigo', component: LancamentoCadastroComponent},
-  { path: 'pessoas', component: PessoasPesquisaComponent},
-  { path: 'pagina-nao-encontrada', component: PaginaNaoEncontradaComponent},
-  { path:'**', redirectTo: 'pagina-nao-encontrada'}
-];
 
 
 @NgModule({
@@ -46,8 +33,7 @@ const routes: Routes = [
     CoreModule,
 
     HttpClientModule,
-    RouterModule.forRoot(routes)
-
+    AppRoutingModule
 
   ],
   providers: [ {provide: LOCALE_ID, useValue: 'pt-BR' },LancamentoService, PessoasService, CategoriasService],
